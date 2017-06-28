2eacb51037161121e5ead4e661ccb9d5836cfcc1
Darjush <darjush96@hotmail.de>
2017-06-28 10:24:58 +0200

    Beautify-Code

--- export.php
+++ export.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Titel</title>
     <?php
-    include("libs/dbconnect.php");
-    include("libs/functions.php");
-    ?>
-  </head>
-	<body>
+include("libs/dbconnect.php");
+include("libs/functions.php");
+?>
+ </head>
+    <body>
 <form action="export.php" method="post" enctype="multipart/form-data">
 <h3>Bitte wählen Sie den AD Export aus.</h3>
 <select name="file">
   <option value="">- Select File -
     <?php
-    include("libs/upload.php");
-    auswahldatei();
-    ?>
+include("libs/upload.php");
+auswahldatei();
+?>
 </select>
 <input type="submit" value="Auswählen">
 </form>
@@ -26,51 +26,50 @@
 $link = dbconn();
 deletedb();
 //  echo $_POST["file"] . "</br>";
-$dataname = $_POST["file"];
+$dataname   = $_POST["file"];
 $fieldnames = 1;
-  if (($handle = fopen("uploads/".$dataname , "r")) !== FALSE) { //Datei mit entsprechend ausgewähltem dateinamen auswähläen
-      while (($data = fgetcsv($handle, 0, ";")) !== FALSE) { //csv auslesen mit , trennung
-          $num = count($data); //Datensätze zählen
-  if($fieldnames !== 0){
-    $fieldnames = 0;
+if (($handle = fopen("uploads/" . $dataname, "r")) !== FALSE) { //Datei mit entsprechend ausgewähltem dateinamen auswähläen
+    while (($data = fgetcsv($handle, 0, ";")) !== FALSE) { //csv auslesen mit , trennung
+        $num = count($data); //Datensätze zählen
+        if ($fieldnames !== 0) {
+            $fieldnames = 0;
 
-  createdb();
-  selectdb($link, 'Adexport');
-      $sql = 'CREATE TABLE Export1'.'(';
-      for ($c=0; $c < $num; $c++){
-        $sql .='`'.$data[$c]. '` varchar(150)';
-        if($c < $num - 1){
-          $sql .= ', ';
+            createdb();
+            selectdb($link, 'Adexport');
+            $sql = 'CREATE TABLE Export1' . '(';
+            for ($c = 0; $c < $num; $c++) {
+                $sql .= '`' . $data[$c] . '` varchar(150)';
+                if ($c < $num - 1) {
+                    $sql .= ', ';
+                }
+
+            } //sql-> Tabelle erstellen
+            $sql .= ')';
+            //  echo $sql . "</br>";
+            $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
         }
 
-      } //sql-> Tabelle erstellen
-      $sql .= ')';
-    //  echo $sql . "</br>";
-      $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
-  }
-  
-  else{
-    $sql = 'INSERT INTO Export1 VALUES (';
-    for ($c=0; $c < $num; $c++) { //für jeden Datensat
-             $sql .= '"'.$data[$c].'"';
-             if($c < $num - 1){
-               $sql .= ', ';
-             }
-      if($c < 0){
-          $sql .= ', ';
-          }//den text ausgeben ($data = Array)
-       }
-       $sql .=');';
+        else {
+            $sql = 'INSERT INTO Export1 VALUES (';
+            for ($c = 0; $c < $num; $c++) { //für jeden Datensat
+                $sql .= '"' . $data[$c] . '"';
+                if ($c < $num - 1) {
+                    $sql .= ', ';
+                }
+                if ($c < 0) {
+                    $sql .= ', ';
+                } //den text ausgeben ($data = Array)
+            }
+            $sql .= ');';
 
-//  echo $sql;
-$result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
-     }
-   }
-        //echo $data[1];
-      }
-        fclose($handle);
-        getusrlognam();
-        cleanID();
-  ?>
+            //  echo $sql;
+            $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
+        }
+    }
+    //echo $data[1];
+}
+fclose($handle);
+getusrlognam();
+?>
 </body>
 </html>
--- libs/functions.php
+++ libs/functions.php
@@ -1,56 +1,45 @@
 <?php
-function deletedb(){
-  $sql = 'DROP TABLE Export1';
-  $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
+function deletedb()
+{
+    $sql    = 'DROP TABLE Export1';
+    $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
 }
 
-function createdb(){
-  $sql = 'CREATE DATABASE ADexport';
-  $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
+function createdb()
+{
+    $sql    = 'CREATE DATABASE ADexport';
+    $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
 }
 
-function getusrlognam(){
-  $sql = 'ALTER TABLE Export1
+function getusrlognam()
+{
+    $sql    = 'ALTER TABLE Export1
   ADD ID varchar(45)';
-  $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
-  $sql = 'SELECT * FROM Export1';
-  $result = mysql_query($sql);
-  //$row;
-  while($row=mysql_fetch_array($result)) {
+    $result = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
+    $sql    = 'SELECT * FROM Export1';
+    $result = mysql_query($sql);
+    //$row;
+    while ($row = mysql_fetch_array($result)) {
         $reallogonname = $row["User Logon Name"];
-        $id = preg_replace('/[^0-9]+/', '', $row["User Logon Name"]); //ersetzt alles durch    ausser zahlen
-       //echo $row["User Logon Name"]."</br>";
-       if(!empty($id)){
-       $sql = "UPDATE Export1
-            Set ID='".$id."' where `User Logon Name`= '".$reallogonname."'";
-              echo $sql."</br>";
-             $result = mysql_query($sql);}
-             else{
-               Echo "lol";
-             }
-      // print_r($row);
-    /*   foreach ($row as $temprow) {
-         $sql = "INSERT INTO Export1 (`ID`)
-                VALUES('" .$temprow['User Logon Name']."')";
-                echo $sql;
-                $result = mysql_query($sql);
-       }*/
-
-
-  }
-  //print_r($rows);
-  //echo $rows[0]['User Logon Name'];
+        $id            = preg_replace('/[^0-9]+/', '', $row["User Logon Name"]); //ersetzt alles durch    ausser zahlen
+        //echo $row["User Logon Name"]."</br>";
+        if (!empty($id)) {
+            $sql = "UPDATE Export1
+            Set ID='" . $id . "' where `User Logon Name`= '" . $reallogonname . "'";
+            echo $sql . "</br>";
+            $result = mysql_query($sql);
+        } else {
+            Echo "lol";
+        }
+    }
 }
 
-function cleanID(){
-
-}
 
 function endsWith($FullStr, $needle)
-    {
-        $StrLen = strlen($needle);
-        $FullStrEnd = substr($FullStr, strlen($FullStr) - $StrLen);
-        return $FullStrEnd == $needle;
-    }
+{
+    $StrLen     = strlen($needle);
+    $FullStrEnd = substr($FullStr, strlen($FullStr) - $StrLen);
+    return $FullStrEnd == $needle;
+}
 
 ?>
