23b62cc10c4244844055d64b8b958c6abc6e84f8
Darjush <darjush96@hotmail.de>
2017-07-04 14:32:58 +0200

    Neue SQL Abfrage

---
+++ libs/teilschulabfrage.php
@@ -0,0 +1,14 @@
+<?php
+function teilschulabfrage($Teilschule){
+  $sql    = 'SELECT * FROM `Export2` WHERE `schule`="'.$Teilschule.'"'; //.$Teilschule."'"
+  $erg = mysql_query($sql); //sql übergeben (Wichtig sonst funzt SQL nicht)
+  $gezählt = mysql_num_rows($erg);
+  //echo $gezählt;
+    if($gezählt !== 0){
+      echo '<input type="checkbox" disabled="disabled" checked="checked">';
+    }
+    else{
+      echo '<input type="checkbox" disabled="disabled">';
+    }
+}
+?>
