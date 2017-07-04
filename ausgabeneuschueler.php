af07af3d4bb6b45638ee64b3a7a9ec60d132e94f
Darjush <darjush96@hotmail.de>
2017-07-04 10:01:14 +0200

    Added Spaltennamen

--- ausgabeneuschueler.php
+++ ausgabeneuschueler.php
@@ -1,4 +1,5 @@
 <?php
 include("libs/neueschueler.php");
         neueschuelerabfrage();
+        
 ?>
--- libs/neueschueler.php
+++ libs/neueschueler.php
@@ -1,6 +1,7 @@
 <?php
 function neueschuelerabfrage(){
   include("libs/dbconnect.php");
+  include("libs/csvspaltennamen.php");
   $link = dbconn();
   selectdb($link, 'Adexport');
   $sql    = "SELECT * FROM Export2 WHERE NOT EXISTS(SELECT NULL FROM Export1 WHERE Export1.verg_id = Export2.verg_id)AND Export2.benutzerstatus = 'Ja'";
@@ -12,10 +13,15 @@ function neueschuelerabfrage(){
     header('Content-Disposition: attachment; filename=NeueschuelerExport.csv');
     // create a file pointer connected to the output stream
     $output = fopen('php://output', 'w');
+/*    $spaltnam = $spaltenname;
+    foreach($spaltnam as $splnm){
+      echo $splnm.";";
+    }*/
+    fputcsv($output,$spaltenname, ";");
 
     while ($row = mysql_fetch_array($resu))
     {
-      $line = array($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9],$row[10],$row[11]);
+      $line = array($row["name"],$row["vorname"],$row["benutzerstatus"],$row["geburtsdatum"],$row["telefon"],$row["mobile"],$row["emailadresse"],$row["SV-Nr"],$row["id"],$row["regelklasse"],$row["profil"],$row["verg_ID"]);
       fputcsv($output, $line, ";");
       }
 
