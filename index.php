57bc5562007ed989f59986f1f32cdabbd71e9a5a
Darjush <darjush96@hotmail.de>
2017-07-04 13:28:14 +0200

    ..

--- index.php
+++ index.php
@@ -7,13 +7,32 @@
   </head>
 
 	<body>
-    <h1>Bitte Wählen Sie den Upload für den Active Directory Export</h1>
+    <h2>Bitte Wählen Sie den Upload für den Active Directory Export</h2>
 		<form action="upload.php" method="post" enctype="multipart/form-data">
 		<input type="file" name="datei"><br>
 		<input type="submit" value="Hochladen">
 		</form>
+  </br></br>
     <form>
-    <h1>Bitte Wählen Sie den Upload für den Kaschuso Export</h1>
+    <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für GIBS</h2>
+    <form action="upload2.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="datei"><br>
+    <input type="submit" value="Hochladen">
+    </form>
+    <form>
+    <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für KBS</h2>
+    <form action="upload2.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="datei"><br>
+    <input type="submit" value="Hochladen">
+    </form>
+    <form>
+    <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für EBZ</h2>
+    <form action="upload2.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="datei"><br>
+    <input type="submit" value="Hochladen">
+    </form>
+    <form>
+    <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für BZGS</h2>
     <form action="upload2.php" method="post" enctype="multipart/form-data">
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
