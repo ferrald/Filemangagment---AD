6ae4bf49dbf0b6b704622ec811a51ab2165b1af1
Darjush <darjush96@hotmail.de>
2017-07-04 14:20:19 +0200

    sm. changes

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Titel</title>
+    <?php
+    include ("libs/dbconnect.php");
+    $link = dbconn();
+    selectdb($link, "ADexport");
+    ?>
   </head>
 
 	<body>
@@ -18,28 +23,46 @@
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
     <?php
-    $Teilschule = "GIBS";
+    include 'libs/teilschulabfrage.php';
+    $Teilschule = "gibsol";
+    teilschulabfrage($Teilschule);
     ?>
     </form>
     <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für KBS</h2>
     <form action="upload2.php" method="post" enctype="multipart/form-data">
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
+    <?php
+    $Teilschule = "kbsol";
+    teilschulabfrage($Teilschule);
+    ?>
     </form>
     <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für EBZ</h2>
     <form action="upload2.php" method="post" enctype="multipart/form-data">
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
+    <?php
+    $Teilschule = "EBZ";
+    teilschulabfrage($Teilschule);
+    ?>
     </form>
     <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für BZGS GB</h2>
     <form action="upload2.php" method="post" enctype="multipart/form-data">
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
+    <?php
+    $Teilschule = "BZGS";
+    teilschulabfrage($Teilschule);
+    ?>
     </form>
     <h2>Bitte Wählen Sie den Upload für den Kaschuso Export für BZGS HF</h2>
     <form action="upload2.php" method="post" enctype="multipart/form-data">
     <input type="file" name="datei"><br>
     <input type="submit" value="Hochladen">
+    <?php
+    $Teilschule = "ddddd";
+    teilschulabfrage($Teilschule);
+    ?>
     </form>
     <form action="funktionwahl.php" mehtod="post" enctype="multipart/form-data">
       <input type="submit" value="Weiter">
