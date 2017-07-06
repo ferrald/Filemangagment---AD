7486b83a297f87536b72bd65899a0723a1d8a2bb
ferrald <ferrald@users.noreply.github.com>
2017-07-06 17:24:42 +0200

    Abfragelogik angepasst
    Ausgabe stimmt noch nicht ganz

--- libs/neueschueler.php
+++ libs/neueschueler.php
@@ -4,9 +4,7 @@ function neueschuelerabfrage(){
   include_once("libs/csvspaltennamen.php");
   $link = dbconn();
   selectdb($link, 'Adexport');
-  $sql    = "SELECT * FROM Export2 WHERE NOT EXISTS(SELECT NULL FROM Export1 WHERE Export1.verg_id = Export2.verg_id)AND Export2.benutzerstatus = 'Ja'";
-  //
-//$sql    = "SELECT * FROM Export2 WHERE NOT EXISTS(SELECT NULL FROM Export1 WHERE Export1.verg_id = Export2.verg_id)"
+  $sql    = "SELECT DISTINCT * FROM `export2` LEFT JOIN `export1` ON export1.`name` = benutzername WHERE `benutzerstatus` = 'Ja' AND `User Logon Name` IS NULL AND `regelklasse` LIKE '%17%'";
     $resu = mysql_query($sql);
     // output headers so that the file is downloaded rather than displayed
     header('Content-Type: text/csv; charset=utf-8');
@@ -22,7 +20,15 @@ function neueschuelerabfrage(){
     while ($row = mysql_fetch_array($resu))
     {
       //$row["benutzerstatus"]
-      $line = array("","",$row["name"],"",$row["vorname"],$row["name"]." ".$row["vorname"],$row["name"]." ".$row["vorname"],$row['name'][0].$row['vorname'][0].$row['verg_ID'],"2wsx","Schueler",$row["verg_ID"],$row["regelklasse"],preg_replace('/[^a-zA-Z]+/', '', $row["id"]);$row["id"],"Aarauerstrasse 30","4600","Olten","","Schweiz","CH",$row["telefon"],"","","",$row["mobile"],"","","","","","","","",$row["emailadresse"],$row['name'][0].$row['vorname'][0].$row['verg_ID']."@bbzolten.ch","","www.bbzolten.so.ch","","","1","1","1","240","","","1","0","","","1","1","1","Users","G_netDomain","G_Win7Office13","Schueler","G_WLanLuS","G_WLanBBZO","","","","","","","","","","","","","","","","","","","","","","","","");
+      $line = array("","",$row["name"],"",$row["vorname"],$row["name"]." ".$row["vorname"],
+				$row["name"]." ".$row["vorname"],$row['name'][0].$row['vorname'][0].$row['verg_ID'],
+				"2wsx","Schueler",$row["verg_ID"],$row["regelklasse"],
+				preg_replace('/[^a-zA-Z]+/', '', $row["id"]),$row["id"],"Aarauerstrasse 30","4600","Olten","","Schweiz","CH",
+				$row["telefon"],"","","",$row["mobile"],"","","","","","","","","E-Mail",
+				$row['name'][0].$row['vorname'][0].$row['verg_ID']."@bbzolten.ch","",
+				"www.bbzolten.so.ch","","","1","1","1","240","","","1","0","","","1","1","1",
+				"Users","G_netDomain","G_Win7Office13","Schueler","G_WLanLuS","G_WLanBBZO",
+				"","","","","","","","","","","","","","","","","","","","","","","","");
       fputcsv($output, $line, ";");
       }
 
