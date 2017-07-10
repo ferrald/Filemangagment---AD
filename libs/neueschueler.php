ec1b8799ef99de939f732505d5438a4b167fddcd
ferrald <ferrald@users.noreply.github.com>
2017-07-10 17:22:50 +0200

    Ausgabe angepasst
    Mailadresse generieren aus benutzername da hier Umlaute bereits ausgefiltert sind, Telefonnummern in andere Spalten

--- libs/neueschueler.php
+++ libs/neueschueler.php
@@ -4,7 +4,7 @@ function neueschuelerabfrage(){
   include_once("libs/csvspaltennamen.php");
   $link = dbconn();
   selectdb($link, 'Adexport');
-  $sql    = "SELECT DISTINCT `export2`.`name` , `export2`.`vorname` , `export2`.`verg_ID` as verg_ID, id, regelklasse, telefon, mobile FROM `export2` LEFT JOIN `export1` ON export1.`name` = benutzername WHERE `benutzerstatus` = 'Ja' AND `User Logon Name` IS NULL AND `regelklasse` LIKE '%17%'";
+  $sql    = "SELECT DISTINCT `export2`.`name` , `export2`.`vorname` , `export2`.`verg_ID` as verg_ID, id, regelklasse, telefon, mobile, benutzername FROM `export2` LEFT JOIN `export1` ON export1.`name` = benutzername WHERE `benutzerstatus` = 'Ja' AND `User Logon Name` IS NULL AND `regelklasse` LIKE '%17%'";
     $resu = mysql_query($sql);
     // output headers so that the file is downloaded rather than displayed
     header('Content-Type: text/csv; charset=utf-8');
@@ -22,9 +22,9 @@ function neueschuelerabfrage(){
       //$row["benutzerstatus"]
       $line = array("","",$row["name"],"",$row["vorname"],$row["name"]." ".$row["vorname"],
 				$row["name"]." ".$row["vorname"],$row['name'][0].$row['vorname'][0].$row['verg_ID'],
-				"2wsx","Schueler",$row["verg_ID"],$row["regelklasse"],
-				preg_replace('/[^a-zA-Z]+/', '', $row["id"]),$row["id"],"Aarauerstrasse 30","4600","Olten","","Schweiz","CH",
-				$row["telefon"],"","","",$row["mobile"],"","","","","","","","","E-Mail",
+				"3edc","Schueler",$row["verg_ID"],$row["regelklasse"],
+				preg_replace('/[^a-zA-Z]+/', '', $row["id"]),"Aarauerstrasse 30","","4600","Olten","","Schweiz","CH",
+				$row["mobile"],"","","","","","","","","",$row["telefon"],"","",$row['benutzername']."@bbzolten.ch",
 				$row['name'][0].$row['vorname'][0].$row['verg_ID']."@bbzolten.ch","",
 				"www.bbzolten.so.ch","","","1","1","1","240","","","1","0","","","1","1","1",
 				"Users","G_netDomain","G_Win7Office13","Schueler","G_WLanLuS","G_WLanBBZO",
